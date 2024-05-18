d59e1a948103658c0c4ae7d52c3e76fe842cdcee
CHAHMADZAFAR <21-SE-92@students.uettaxila.edu.pk>
2024-05-18 23:58:15 +0500

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "CHAUDAHRY AHMAD ZAFAR",
+  title: "Public Speaker,Founder ",
+  email: "chahmadzafar@outlook.com",
+  gitHub: "CHAHAMDZAFAR",
+  instagram: "",
+  linkedIn: "CH AHMAD ZAFAR",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "CH_AHMAD_ZAFAR",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
